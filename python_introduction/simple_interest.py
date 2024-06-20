c773adfd481aae75ae648b8054cc1b8eb12953e2
DannyNak2 <danielnakachew1@gmail.com>
2024-06-20 22:53:19 +0300

    created the simple interest fiel

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Define the three variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Perform the arithmetic operations
+interest = principal * rate * time
+
+# Print the results in the specified format
+print(f"The simple interest is: {interest}")
