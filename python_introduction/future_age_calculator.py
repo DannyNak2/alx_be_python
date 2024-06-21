bd7fe7d3d530194d664ae2fb768e5462e8e851f1
DannyNak2 <danielnakachew1@gmail.com>
2024-06-21 11:34:00 +0300

    creaed future_age_calculator file

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# Define the one variable
+current_age = int(input("How old are you?"))
+
+# Perform the arthimetic operation
+future_age = current_age + 27
+
+# Print the results in the specified format
+print(f"In 2050, you will be {future_age} years old.")
