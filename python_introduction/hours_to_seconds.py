79d45490d9b677296d767ea7dcac3c2240800367
DannyNak2 <danielnakachew1@gmail.com>
2024-06-21 11:17:44 +0300

    Created the hours_to _seconds file

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Define the one variable
+hours = 2
+
+# Perform the calculation(arthimetic operation)
+seconds = hours * 3600
+
+# Print the result in a specified format
+print(f"{hours} hour(s) is {seconds} seconds.")
