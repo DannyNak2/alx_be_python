1fd9af5c78af5de0b3d251a7946daba9b3b9f884
DannyNak2 <danielnakachew1@gmail.com>
2024-06-30 00:23:20 +0300

    add name second

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 # Define the Variables to get user input for first and second number
 num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 # Define the Variabel to get user input for the operaton
 operation = input("Choose the operation (+, -, *, /): ")
@@ -24,3 +24,7 @@ match operation:
             print(f"The result is {result:.2f}.")
     case _:
         print("Invalid operation. Please choose +, -, *, or /.")
+
+        git add .
+        git commit -m"add the name second"
+        git push origin main
